8eb46ceafedc0c8e208fd1879b02df5bdff3db32
theprogmatheus <theprog.matheus@gmail.com>
2025-03-28 14:54:51 -0300

    Create scrap() function on sigaa.js

--- assets/js/sigaa.js
+++ assets/js/sigaa.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+/*
+*  Função para extrair as informações das disciplinas.
+*/
+function scrap() {
+    let disciplinas = document.getElementById("turmas-portal")?.children[2]?.children[1];
+    console.log(disciplinas)
+    if (disciplinas) {
+        let resultObject = [];
+        for (let element of disciplinas.children) {
+            let texts = element?.innerText;
+            if (texts) {
+                let lines = texts.split("\n").map(text => text.trim()).filter(text => text.length > 0);
+                if (lines.length > 0) {
+                    let name = lines[0];
+                    let location = lines[1];
+                    let schedules = lines[2].split(" ");
+
+                    resultObject.push({
+                        name,
+                        location,
+                        schedules
+                    })
+                }
+            }
+        }
+        return resultObject;
+    }
+}
+
+console.log(scrap())
