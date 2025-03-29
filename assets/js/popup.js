ef99743a7daf2199963c2d894650075f84f276bd
theprogmatheus <theprog.matheus@gmail.com>
2025-03-29 11:09:15 -0300

    Fix: Could not establish connection. Receiving end does not exist.

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -3,6 +3,9 @@ import { parse } from "./h.js";
 document.addEventListener("DOMContentLoaded", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { action: "scrap.sigaa" }, (response) => {
+            if (chrome.runtime.lastError)
+                return;
+            
             let payload = response?.payload;
             if (payload) {
 
