b2621a87bd5e34261f7800c7f6bd511929cd2240
theprogmatheus <theprog.matheus@gmail.com>
2025-03-29 10:41:32 -0300

    Finished render()

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -13,8 +13,57 @@ document.addEventListener("DOMContentLoaded", function () {
                     return data;
                 })
 
+                render(data);
+
                 console.log("Data:", data)
             }
         });
     });
 });
+
+function render(data) {
+    if (data) {
+        let contentDiv = document.getElementById("content");
+        data.forEach(element => {
+
+            let rowDiv = document.createElement("div");
+            rowDiv.className = "row";
+
+            let nameDiv = document.createElement("div");
+            nameDiv.className = "name";
+
+            let locationDiv = document.createElement("div");
+            locationDiv.className = "location";
+
+            let scheduleDiv = document.createElement("div");
+            scheduleDiv.className = "schedule"
+
+            let nameSpan = document.createElement("span");
+            nameSpan.innerText = element.name;
+
+            let locationSpan = document.createElement("span");
+            locationSpan.innerText = element.location;
+
+            let scheduleSpans = element.schedules.map(schedule => {
+                let scheduleSpan = document.createElement("span");
+                scheduleSpan.innerText = schedule;
+                return scheduleSpan;
+            });
+
+
+            nameDiv.appendChild(nameSpan);
+            locationDiv.appendChild(locationSpan);
+            scheduleSpans.forEach(element => {
+                scheduleDiv.appendChild(element);
+            });
+
+
+            rowDiv.appendChild(nameDiv);
+            rowDiv.appendChild(locationDiv);
+            rowDiv.appendChild(scheduleDiv);
+
+
+            contentDiv.appendChild(rowDiv);
+        });
+    }
+}
